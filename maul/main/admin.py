aaa7bdb6464afa729ae523d0427b0819ea1067eb
maxnotmin <mikemax@outlook.com>
2019-12-03 19:25:26 -0700

    added admin and a model

--- maul/main/admin.py
+++ maul/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Services
 # Register your models here.
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    fields = ["service_title", "service_content", "service_published"]
+
+
+admin.site.register(Services, ServiceAdmin)
--- maul/main/models.py
+++ maul/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.timezone import now
+
 
 # Create your models here.
+class Services(models.Model):
+    # Create a new model
+    # Here we are description what we want our data to look like, what we may need
+    # Each model will map to a database table
+    # The default DB is Sqlite3
+    # TO changes to the dir maul > settings.py > DATABASE
+
+    # Primary key is set by default
+    #CharField is a shorter field
+    service_title = models.CharField(max_length=200)
+    #TextField used for longer text items
+    service_content = models.TextField()
+    service_published = models.DateTimeField("date published")
+
