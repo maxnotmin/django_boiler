1dae1b30d2a9a93cb045608586db3c04392cba81
maxnotmin <mikemax@outlook.com>
2019-12-15 13:39:32 -0700

    added reg

--- maul/main/views.py
+++ maul/main/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Services
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def homepage(request):
     return render(request=request,
                   template_name="main/home.html",
                   context={"services": Services.objects.all})
+
+
+def register(request):
+    form = UserCreationForm
+    return render(request=request,
+                  template_name="main/register.html",
+                  context={"form": form})
